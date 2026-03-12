c7ae94d9af96c9e10566b8ed25b070c5538ed648
Glenn444 <gmark1586@gmail.com>
2026-03-12 10:15:25 +0300

    Write more king tests

--- cli/internal/pieces/king.go
+++ cli/internal/pieces/king.go
@@ -2,6 +2,8 @@ package pieces
 
 import (
 	"fmt"
+
+	"github.com/Glenn444/golang-chess/utils"
 )
 
 type King struct {
@@ -31,7 +33,12 @@ func (k King) GetLegalSquares(g GameState) []string {
 		// check bounds: file 0–7, rank 1–8
 		if newFile >= 0 && newFile < 8 && newRank >= 1 && newRank <= 8 {
 			move := string(files[newFile]) + fmt.Sprint(newRank)
+			row,col := utils.Chess_notation_to_indices(move)
+			if g.Board[row][col].Occupied && g.Board[row][col].Piece.GetColor() != k.Color{
 			moves = append(moves, move)
+			}else if !g.Board[row][col].Occupied{
+				moves = append(moves, move)
+			}
 		}
 	}
 
--- cli/internal/pieces/king_test.go
+++ cli/internal/pieces/king_test.go
@@ -11,12 +11,12 @@ import (
 func TestKingPiece(t *testing.T){
 	initialBoard := map[string]string{
 		// White pieces
-		"a2": "P", "b3": "P", "c6": "P", "d2": "P", "e2": "P", "f2": "P", "g2": "P", "h2": "P",
+		"a2": "P", "b3": "P", "c3": "P", "d2": "P", "e2": "P", "f2": "P", "g2": "P", "h2": "P",
 		"a1": "R", "h1": "R",
 		"b1": "N", "g1": "N",
 		"e4": "B", "f1": "B",
 		"d1": "Q",
-		"e1": "K",
+		"c5": "K",
 
 		// Black pieces
 		"a7": "p", "b7": "p", "c7": "p", "d7": "p", "e5": "p", "f7": "p", "g7": "p", "h7": "p",
@@ -27,9 +27,11 @@ func TestKingPiece(t *testing.T){
 		"e8": "k",
 	}
 
+	chessBoard :=  SetUpBoard(initialBoard)
+
 	gameState := GameState{
 		CurrentPlayer: "w",
-		Board:         SetUpBoard(initialBoard),
+		Board:        chessBoard,
 	}
 	kingTests := []struct {
 		name         string
@@ -37,21 +39,22 @@ func TestKingPiece(t *testing.T){
 		legalSquares []string
 	}{
 		{
-			name:         "Ke1",
-			position:     "e1",
-			legalSquares: []string(nil),
+			name:         "Kc5",
+			position:     "c5",
+			legalSquares: []string{"d5", "b5", "c6", "c4", "d6", "d4", "b6", "b4"},
 		},
 		{
 			name:         "Ke8",
 			position:     "e8",
-			legalSquares: []string(nil),
+			legalSquares: []string{"e7"},
 		},
 		
 	}
 
+	PrintBoard(chessBoard)
 	for _, squares := range gameState.Board {
 		for _, square := range squares {
-			if square.Piece.GetPieceType() == "K" {
+			if square.Occupied && square.Piece.GetPieceType() == "K" {
 				for _, ktest := range kingTests {
 					if square.Piece.GetPosition() == ktest.position {
 						t.Run(ktest.name, func(t *testing.T) {
